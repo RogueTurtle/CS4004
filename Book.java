17bdeec3ee9c2cc300f25ec09673762843652e0b
wildfowlowl <45610466+wildfowlowl@users.noreply.github.com>
2022-11-19 22:16:08 +0000

    Add files via upload

---
+++ Book.java
@@ -0,0 +1,43 @@
+// Java Program to Illustrate Book Class that
+// takes Input from the Books and related information
+
+package main;
+
+// Importing required classes
+import java.util.Scanner;
+
+// Class
+public class Book {
+
+	// Class data members
+	public int sNo;
+	public String BookName;
+	public String authorName;
+	public int BookQty;
+	public int BookQtyCopy;
+
+	// Creating object of Scanner class to
+	// read input from users
+	Scanner input = new Scanner(System.in);
+
+	// Method
+	// To add Book details
+	public Book() {
+		// Display message for taking input later
+		// taking input via
+		// nextInt() and nextLine() standard methods
+		System.out.println("Enter Serial No of Book:");
+		this.sNo = input.nextInt();
+		input.nextLine();
+
+		System.out.println("Enter Book Name:");
+		this.BookName = input.nextLine();
+
+		System.out.println("Enter Author Name:");
+		this.authorName = input.nextLine();
+
+		System.out.println("Enter Quantity of Books:");
+		this.BookQty = input.nextInt();
+		BookQtyCopy = this.BookQty;
+	}
+}
---
+++ Students.java
@@ -0,0 +1,155 @@
+// Java Program to Illustrate Students Class
+// To Do all the Operations related to Students:
+// add Students,check-in books,check out books,ValidStudent
+package main;
+
+import java.time.LocalDate;
+// Importing required classes
+import java.util.Scanner;
+
+// Class
+public class Students {
+
+	// Creating objects of Scanner and Students class
+	Scanner input = new Scanner(System.in);
+	Student theStudents[] = new Student[50];
+
+	public static int count = 0;
+
+	// Method 1
+	// To add Books
+	public void addStudent(Student s) {
+		for (int i = 0; i < count; i++) {
+
+			if (s.studentIdNum.equalsIgnoreCase(
+					theStudents[i].studentIdNum)) {
+
+				// Print statement
+				System.out.println(
+						"Student of Id Number: " + s.studentIdNum
+								+ " is Already Registered.");
+
+				return;
+			}
+		}
+
+		if (count <= 50) {
+			theStudents[count] = s;
+			count++;
+		}
+	}
+
+	// Method 2
+	// Displaying all Students
+	public void showAllStudents() {
+		// Printing Student name and
+		// corresponding registered number
+		System.out.println("Student Name\t\tId Number");
+		for (int i = 0; i < count; i++) {
+
+			System.out.println(theStudents[i].studentName
+					+ "\t\t"
+					+ theStudents[i].studentIdNum);
+		}
+	}
+
+	// Method 3
+	// To check the Student
+	public int isStudent() {
+		// Display message only
+		System.out.println("Enter Reg Number:");
+
+		String regNum = input.nextLine();
+
+		for (int i = 0; i < count; i++) {
+
+			if (theStudents[i].studentIdNum.equalsIgnoreCase(
+					regNum)) {
+				return i;
+			}
+		}
+
+		// Print statements
+		System.out.println("Student is not Registered.");
+		System.out.println("Get Registered First.");
+
+		return -1;
+	}
+
+	// Method 4
+	// To remove the Book
+	public void checkOutBook(Books Book) {
+		int StudentIndex = this.isStudent();
+
+		if (StudentIndex != -1) {
+			System.out.println("checking out");
+
+			Book.showAllBooks();
+			Book b = Book.checkOutBook();
+
+			System.out.println("checking out");
+			if (b != null) {
+
+				if (theStudents[StudentIndex].BooksCount <= 3) {
+
+					System.out.println("adding Book");
+					theStudents[StudentIndex].borrowedBooks[theStudents[StudentIndex].BooksCount] = b;
+					theStudents[StudentIndex].BooksCount++;
+
+					return;
+				} else {
+
+					System.out.println(
+							"Student Can not Borrow more than 3 Books.");
+					return;
+				}
+			}
+			System.out.println("Book is not Available.");
+		}
+	}
+
+	// Method 5
+	// To add the Book
+	public void checkInBook(Books Book) {
+		int StudentIndex = this.isStudent();
+		if (StudentIndex != -1) {
+
+			// Printing credentials corresponding to Student
+			System.out.println(
+					"S.No\t\t\tBook Name\t\t\tAuthor Name");
+
+			Student s = theStudents[StudentIndex];
+
+			for (int i = 0; i < s.BooksCount; i++) {
+
+				System.out.println(
+						s.borrowedBooks[i].sNo + "\t\t\t"
+								+ s.borrowedBooks[i].BookName + "\t\t\t"
+								+ s.borrowedBooks[i].authorName);
+			}
+
+			// Display message only
+			System.out.println(
+					"Enter Serial Number of Book to be Checked In:");
+
+			int sNo = input.nextInt();
+
+			for (int i = 0; i < s.BooksCount; i++) {
+				if (sNo == s.borrowedBooks[i].sNo) {
+					Book.checkInBook(s.borrowedBooks[i]);
+					s.borrowedBooks[i] = null;
+
+					return;
+				}
+			}
+
+			System.out.println("Book of Serial No " + sNo
+					+ "not Found");
+		}
+	}
+
+	public void overdueCheck() {
+		LocalDate nowDate = LocalDate.now();
+
+	}
+}
