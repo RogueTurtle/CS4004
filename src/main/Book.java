e03c95dfc016c85430571caeffab0f7300ac3dd4
wildfowlowl <45610466+wildfowlowl@users.noreply.github.com>
2022-11-20 22:00:18 +0000

    Update Book.java
    Added comments

--- src/main/Book.java
+++ src/main/Book.java
@@ -1,31 +1,22 @@
-// Java Program to Illustrate Book Class that
-// takes Input from the Books and related information
-
 package main;
 
-// Importing required classes
 import java.util.Scanner;
 
-// Class
 public class Book {
 
-	// Class data members
+	// Variables
 	public int sNo;
 	public String BookName;
 	public String authorName;
 	public int BookQty;
 	public int BookQtyCopy;
 
-	// Creating object of Scanner class to
-	// read input from users
+	// Scanner for user input
 	Scanner input = new Scanner(System.in);
 
-	// Method
-	// To add Book details
+	// Add Book
 	public Book() {
-		// Display message for taking input later
-		// taking input via
-		// nextInt() and nextLine() standard methods
+		// Setting variables via user input
 		System.out.println("Enter Serial No of Book:");
 		this.sNo = input.nextInt();
 		input.nextLine();
